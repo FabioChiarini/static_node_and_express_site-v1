f1f22cc0bceeed1fea943581eacd026d1f574c74
Fabio Chiarini <chiarrss@gmail.com>
2019-04-25 19:59:19 +0200

    Basic error handling now functioning

--- app.js
+++ app.js
@@ -5,16 +5,36 @@ const app = express();
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
+
+app.use((req, res, next) => {
+    const err = new Error('Oh no!!');
+    err.status = 500;
+    next(err);
+});
+
+
 //create routes to display index and about pages
 const mainRoutes = require('./routes');
 //create routes to display the selected project
 const projectsRoutes = require('./routes/projects');
 
-//console.log(projects_list.projects[2].image_urls[0]);
 
 app.use(mainRoutes);
 app.use('/projects', projectsRoutes);
 
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found!');
+    err.status = 404;
+    next(err);
+});
+
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
 
 app.listen(3000, () => {
     console.log('Application running on localhost:3000');
